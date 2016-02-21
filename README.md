fa9303ff9d05763f53761f5e061cd613b90fdfa9
Spencer Kelly <spencermountain@gmail.com>
2016-02-21 11:47:14 -0800

    sms/wikipedia & sotu corpus

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 
+#state of the union
+American presidential speech transcrips from 2000-2015.
 ```javascript
 const corpus = require("nlp-corpus")
-corpus["state of the union"].bush1986
+let txt= corpus["state of the union"]["2003"]
+// "Mr. Speaker, Vice President Cheney, Members of Congress, ..."
+```
+
+#SMS Corpus
+js version of the National University of Singapore's [56k SMS message  Corpus](http://wing.comp.nus.edu.sg:8080/SMSCorpus/overview.jsp)
+
+```javascript
+const corpus = require("nlp-corpus")
+let txt= corpus["sms"][491];
+// "Wat would u like 4 ur birthday?"
+```
+
+#Wikipedia
+quickly build a plaintext corpus, in any language, using the 1000 most generic ['articles_every_Wikipedia_should_have'](https://meta.wikimedia.org/wiki/List_of_articles_every_Wikipedia_should_have).
+```javascript
+const corpus = require("nlp-corpus")
+//    wikipedia.build(lang, article_count)
+corpus.wikipedia.build("fr", 20)
+// .. wait a couple seconds for it to finish
+```
+```bash
+head  ./wikipedia/corpus/fr.txt
+# Albert Einstein, né le 14 mars 1879, est un physicien théoricien qui fut...
+wc -w  ./wikipedia/corpus/fr.txt
+# [some large number of words]
 ```
--- wikipedia/build.js
+++ wikipedia/build.js
@@ -1,8 +1,60 @@
 'use strict';
+const pages = require('./pages');
+const wtf_wikipedia = require('wtf_wikipedia');
+const async = require('async');
+const fs = require('fs');
+
+//Fisher-Yates array randomizer
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
 
 const build = function(lang, size) {
+  lang = lang || 'en';
+  if (size > pages.length) {
+    size = pages.length;
+  }
+  let articles = shuffle(pages).slice(0, size);
+  articles = articles.map((o) => o[lang]);
 
+  const fetch_page = function(title, cb) {
+    wtf_wikipedia.from_api(title, lang, function(markup) {
+      let text = wtf_wikipedia.plaintext(markup) || '';
+      cb(null, text);
+    });
+  };
+
+  async.mapLimit(articles, 5, fetch_page, (err, result) => {
+    let txt = result.reduce((s, page) => {
+      if (page && typeof page === 'string') {
+        s += page + '\n';
+      }
+      return s;
+    }, {});
+    //write it to a file
+    let filename = __dirname + '/corpus/' + lang + '.txt';
+    fs.writeFile(filename, txt, 'utf8', () => {
+      console.log('---done!');
+      console.log('view file at: ');
+      console.log(filename);
+      console.log('use \'head\' or \'wc -w\' if it is too big to open');
+    });
+  });
   return;
 };
 
 module.exports = build;
+
+build('fr', 5);
