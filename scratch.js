a786af9d0a07acef3d84dd94374ad19d587377d5
spencer kelly <spencerkelly86@gmail.com>
2021-05-07 09:07:08 -0400

    add jeopardy questions data

--- scratch.js
+++ scratch.js
@@ -1,4 +1,14 @@
-const corpus = require('./src');
+// const corpus = require('./src')
 
-let str = corpus.generate(12000); //2k words
-console.log(str);
+// let str = corpus.generate(12000) //2k words
+// console.log(str)
+const data = require('/Users/spencer/data/corpus/JEOPARDY_QUESTIONS1.json')
+data.slice(0, 20000).forEach(o => {
+  let str = o.question
+  if (str.match('<') || str.match('__') || str.match(' x ') || str.length < 20) {
+    return
+  }
+  str = str.replace(/^'/, '')
+  str = str.replace(/'$/, '')
+  console.log(str)
+})
