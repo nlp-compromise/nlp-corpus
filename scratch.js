726384f56e83426be1f23ffdcaea36c933656879
spencer kelly <spencerkelly86@gmail.com>
2021-05-07 09:18:08 -0400

    add wikihow to corpus

--- scratch.js
+++ scratch.js
@@ -2,13 +2,14 @@
 
 // let str = corpus.generate(12000) //2k words
 // console.log(str)
-const data = require('/Users/spencer/data/corpus/JEOPARDY_QUESTIONS1.json')
-data.slice(0, 20000).forEach(o => {
-  let str = o.question
-  if (str.match('<') || str.match('__') || str.match(' x ') || str.length < 20) {
-    return
-  }
-  str = str.replace(/^'/, '')
-  str = str.replace(/'$/, '')
-  console.log(str)
-})
+const fs = require('fs')
+let data = fs.readFileSync('/Users/spencer/data/corpus/wikihowAll.csv').toString()
+data = data.split(/\n/)
+
+// let data = require('')
+
+console.log(data.length)
+data = data.map(str => str.replace(/^, /, ''))
+data = data.map(str => str.replace(/,$/, ''))
+data = data.filter(str => str)
+console.log(data.length)
