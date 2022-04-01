4efe82948f58db5d9b9cbd110b69e215a29011a0
spencer kelly <spencerkelly86@gmail.com>
2022-04-01 15:57:38 -0400

    some frequency analysis results

--- scripts/freq.js
+++ scripts/freq.js
@@ -8,42 +8,56 @@ const size = 10000
 
 let arr = corpus()
 
+let want = {
+  'be ': true,
+  'me ': true,
+  'years': true,
+  'ein': true,
+  'siz': true,
+  'rea': true,
+}
+
 let obj = {}
 arr.forEach(txt => {
   let doc = nlp(txt).compute('root')
-  doc.docs.forEach(terms => {
-    terms.forEach(t => {
-      if (t.root === 'probable') {
-        console.log(t)
-      }
-      if (t.root) {
-        obj[t.root] = obj[t.root] || 0
-        obj[t.root] += 1
-      }
-    })
+  doc.terms().forEach(t => {
+    // if (t.has('(#ProperNoun|#Conjunction|#Preposition|#Auxiliary|#Modal|#Pronoun|#Determiner|#Expression|#Value|#Copula|#QuestionWord|#Time|#Negative|#Abbreviation)')) {
+    //   return
+    // }
+    if (t.has('(#Value|#Time|#Abbreviation)')) {
+      return
+    }
+    let str = t.text('root')
+    // if (want[str] === true) {
+    //   console.log(txt)
+    // }
+    if (str.length > 1 || str === 'i' || str === 'a') {
+      obj[str] = obj[str] || 0
+      obj[str] += 1
+    }
   })
 })
 
-// let all = Object.entries(obj)
-// all = all.filter(a => a[1] >= min)
-// all = all.sort((a, b) => {
-//   if (a[1] > b[1]) {
-//     return -1
-//   } else if (a[1] < b[1]) {
-//     return 1
-//   }
-//   return 0
-// })
-// all = all.map((a, i) => [a[0], a[1], i + 1])
+let all = Object.entries(obj)
+all = all.filter(a => a[1] >= min)
+all = all.sort((a, b) => {
+  if (a[1] > b[1]) {
+    return -1
+  } else if (a[1] < b[1]) {
+    return 1
+  }
+  return 0
+})
+all = all.map((a, i) => [a[0], a[1], i + 1])
 
-// all = all.slice(0, size)
+all = all.slice(0, size)
 
-// let out = all.reduce((h, a) => {
-//   h += JSON.stringify(a) + ',\n'
-//   return h
-// }, '')
-// out = `export default [\n${out}\n]`
+let out = all.reduce((h, a) => {
+  h += JSON.stringify(a) + ',\n'
+  return h
+}, '')
+out = `export default [\n${out}\n]`
 
-// fs.writeFileSync('./out.js', out)
+fs.writeFileSync('./out.js', out)
 
-// console.log(all.length)
+console.log(all.length)
