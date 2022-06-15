a529d8f176e969a784627a5a20621dc9820dcc91
spencer kelly <spencerkelly86@gmail.com>
2022-06-15 10:05:02 -0400

    add more sentences

--- scratch.js
+++ scratch.js
@@ -1,6 +1,16 @@
 import fs from 'fs'
 import path from 'path'
-import hash from '/Users/spencer/mountain/compromise/src/1-one/output/api/lib/hash.js'
+
+let files = [
+  '/Users/spencer/mountain/corpus/more/res/country.txt',
+  '/Users/spencer/mountain/corpus/more/res/enron.txt',
+  '/Users/spencer/mountain/corpus/more/res/jeopardy.txt',
+  '/Users/spencer/mountain/corpus/more/res/pop.txt',
+  '/Users/spencer/mountain/corpus/more/res/wikihow.txt',
+]
+let allDocs = files.map(file => {
+  return fs.readFileSync(file).toString().split('\n')
+})
 
 const dir = new URL('./', import.meta.url).pathname
 const total = 99
@@ -10,26 +20,24 @@ const getDoc = function (i) {
   return JSON.parse(fs.readFileSync(file).toString())
 }
 
+const getOne = function () {
+  let x = Math.floor(Math.random() * allDocs.length)
+  let y = Math.floor(Math.random() * allDocs[x].length)
+  return allDocs[x][y]
+}
+
 let already = {}
 let sum = 0
 let newSum = 0
 for (let i = total - 1; i >= 0; i -= 1) {
   let json = getDoc(i)
   console.log('\n#' + i + '  - ' + json.length)
-  sum += json.length
-  json = json.filter(str => {
-    let h = hash(str)
-    if (already[h]) {
-      return false
-    }
-    already[h] = true
-    return true
-  })
-  newSum += json.length
+  while (json.length < 1000) {
+    let str = getOne()
+    json.push(str)
+  }
   console.log('  -> ' + json.length)
   let file = path.join(dir, `./builds/doc-${i}.json`)
-  // fs.writeFileSync(file, JSON.stringify(json, null, 2))
+  fs.writeFileSync(file, JSON.stringify(json, null, 2))
 }
 console.log('\n\n-----')
-console.log(sum)
-console.log(newSum)
